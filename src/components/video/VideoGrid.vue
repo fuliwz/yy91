47f7b3ff5cc6652c652247b720b3f3fdb07596a0
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-21 17:20:53 +0800

    feat: add video grid component for yy91 v2

---
+++ src/components/video/VideoGrid.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="video-grid">
+    <VideoCard v-for="item in items" :key="item.id" :item="item" />
+  </section>
+</template>
+
+<script setup>
+import VideoCard from './VideoCard.vue'
+defineProps({ items: { type: Array, default: () => [] } })
+</script>
