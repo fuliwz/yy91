6a3f2ac8096a8ac7bd74a68275f86883087f2523
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-21 23:15:02 +0800

    cleanup router for cloudflare pages build stability

--- src/router/index.js
+++ src/router/index.js
@@ -3,25 +3,19 @@ import Home from '../views/Home.vue'
 import Play from '../views/Play.vue'
 import Category from '../views/Category.vue'
 import Search from '../views/Search.vue'
-import Novel from '../views/Novel.vue'
-import NovelRead from '../views/NovelRead.vue'
-import Image from '../views/Image.vue'
 
 const routes = [
   { path: '/', name: 'home', component: Home },
   { path: '/video/:id?', name: 'video', component: Category, props: true },
   { path: '/play/:id', name: 'play', component: Play, props: true },
-  { path: '/search', name: 'search', component: Search },
-  { path: '/novel', name: 'novel', component: Novel },
-  { path: '/novel/read/:id', name: 'novel-read', component: NovelRead, props: true },
-  { path: '/image/:id?', name: 'image', component: Image, props: true }
+  { path: '/search', name: 'search', component: Search }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-  scrollBehavior(){
-    return { top:0, left:0 }
+  scrollBehavior () {
+    return { top: 0, left: 0 }
   }
 })
 
