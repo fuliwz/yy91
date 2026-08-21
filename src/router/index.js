ea490cb60e3bc19604409398e00b04509bd102ca
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-21 16:27:19 +0800

    refactor: rebuild route structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,52 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Play from '../views/Play.vue'
-import Category from '../views/Category.vue'
-import Search from '../views/Search.vue'
-const routes=[{path:'/',name:'home',component:Home},{path:'/play/:id',name:'play',component:Play,props:true},{path:'/category/:id/:page?',name:'category',component:Category,props:true},{path:'/search',name:'search',component:Search}]
-const router=createRouter({history:createWebHistory(),routes,scrollBehavior(){return{top:0,left:0}}})
-export default router
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('../views/Home.vue'),
+    meta: { title: 'YY91 - 首页' }
+  },
+  {
+    path: '/category/:id/:page?',
+    name: 'category',
+    component: () => import('../views/Category.vue'),
+    props: true,
+    meta: { title: '分类列表' }
+  },
+  {
+    path: '/play/:id',
+    name: 'play',
+    component: () => import('../views/Play.vue'),
+    props: true,
+    meta: { title: '在线播放' }
+  },
+  {
+    path: '/search',
+    name: 'search',
+    component: () => import('../views/Search.vue'),
+    meta: { title: '搜索' }
+  },
+  {
+    path: '/novel/:type?',
+    name: 'novel',
+    component: () => import('../views/Novel.vue'),
+    props: true,
+    meta: { title: '小说' }
+  },
+  {
+    path: '/read/:id',
+    name: 'read',
+    component: () => import('../views/Read.vue'),
+    props: true,
+    meta: { title: '阅读' }
+  }
+]
+
+export default createRouter({
+  history: createWebHistory(),
+  routes,
+  scrollBehavior(){
+    return { top: 0 }
+  }
+})
