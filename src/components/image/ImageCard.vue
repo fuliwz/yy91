6213e6521f1802b94b28b1bcd1d583baaaafdd89
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-21 17:26:14 +0800

    feat: add yy91 v2 image card component

---
+++ src/components/image/ImageCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <article class="image-card" @click="$emit('click', item)">
+    <img :src="item.cover || '/fallback.jpg'" :alt="item.title" loading="lazy" />
+    <h3>{{ item.title }}</h3>
+  </article>
+</template>
+
+<script setup>
+defineProps({
+  item: {
+    type: Object,
+    default: () => ({})
+  }
+})
+
+defineEmits(['click'])
+</script>
