708588d05a3165cf60cbb5b3f8f84c3b6ae314f5
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-21 17:23:29 +0800

    refactor router for yy91 v2 content architecture

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,26 @@ import Home from '../views/Home.vue'
 import Play from '../views/Play.vue'
 import Category from '../views/Category.vue'
 import Search from '../views/Search.vue'
-const routes=[{path:'/',name:'home',component:Home},{path:'/play/:id',name:'play',component:Play,props:true},{path:'/category/:id/:page?',name:'category',component:Category,props:true},{path:'/search',name:'search',component:Search}]
-const router=createRouter({history:createWebHistory(),routes,scrollBehavior(){return{top:0,left:0}}})
+import Novel from '../views/Novel.vue'
+import NovelRead from '../views/NovelRead.vue'
+import Image from '../views/Image.vue'
+
+const routes = [
+  { path: '/', name: 'home', component: Home },
+  { path: '/video/:id?', name: 'video', component: Category, props: true },
+  { path: '/play/:id', name: 'play', component: Play, props: true },
+  { path: '/search', name: 'search', component: Search },
+  { path: '/novel', name: 'novel', component: Novel },
+  { path: '/novel/read/:id', name: 'novel-read', component: NovelRead, props: true },
+  { path: '/image/:id?', name: 'image', component: Image, props: true }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+  scrollBehavior(){
+    return { top:0, left:0 }
+  }
+})
+
 export default router
