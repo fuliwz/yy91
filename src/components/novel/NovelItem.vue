f72ffbe0bc42915e0558506cfaeeefb7d2d5b46e
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-21 17:20:57 +0800

    feat: add novel item component for yy91 v2

---
+++ src/components/novel/NovelItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="novel-item" @click="read">
+    <h3>{{ item.title }}</h3>
+    <p>{{ item.description || '暂无简介' }}</p>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+const props = defineProps({ item: { type: Object, default: () => ({}) } })
+const router = useRouter()
+const read = () => router.push(`/novel/read/${props.item.id}`)
+</script>
