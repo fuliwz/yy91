cbcc6a011cfc463c53679d7e482ab30a0588b762
fuliwz <92189180+fuliwz@users.noreply.github.com>
2026-08-21 17:19:36 +0800

    refactor: add unified player component

---
+++ src/components/video/Player.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="player-container">
+    <video ref="video" controls playsinline></video>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const video = ref(null)
+defineExpose({ video })
+</script>
